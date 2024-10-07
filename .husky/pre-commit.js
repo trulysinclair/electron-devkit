f4e55acfe7e25ac03ae115325aeac7d3857a6ae1
Sinclair <truly.sinclair@gmail.com>
2024-10-07 18:58:53 -0400

    refactor: only test source file changes

--- .husky/pre-commit.js
+++ .husky/pre-commit.js
@@ -1,31 +1,22 @@
 import { execSync } from 'node:child_process'
 
-// const pathsToCheck = [
-//   'app',
-//   'commands',
-//   'config',
-//   'database',
-//   'inertia',
-//   'resources',
-//   'start',
-//   'tests',
-//   'types',
-//   'util',
-// ]
-//
-// const hasChangesInPaths = (changedFiles, paths) =>
-//   paths.some((path) => changedFiles.some((file) => file.startsWith(path)))
-//
-// const unstagedFiles = execSync('git diff --name-only').toString().split('\n').filter(Boolean)
-// const stagedFiles = execSync('git diff --cached --name-only').toString().split('\n').filter(Boolean)
-// const allChangedFiles = [...new Set([...unstagedFiles, ...stagedFiles])]
-// const shouldRunTests = hasChangesInPaths(allChangedFiles, pathsToCheck)
-//
-// if (shouldRunTests) {
-//   console.log('Testing changed files')
-//   execSync('yarn test', { stdio: 'inherit' })
-// } else {
-//   console.log('No files to test')
-// }
+const pathsToCheck = [
+  'packages',
+  'apps',
+  'shared',
+]
 
-execSync('yarn test', { stdio: 'inherit' })
+const hasChangesInPaths = (changedFiles, paths) =>
+  paths.some((path) => changedFiles.some((file) => file.startsWith(path)&&file.match(/\.tsx?/g)))
+
+const unstagedFiles = execSync('git diff --name-only').toString().split('\n').filter(Boolean)
+const stagedFiles = execSync('git diff --cached --name-only').toString().split('\n').filter(Boolean)
+const allChangedFiles = [...new Set([...unstagedFiles, ...stagedFiles])]
+const shouldRunTests = hasChangesInPaths(allChangedFiles, pathsToCheck)
+
+if (shouldRunTests) {
+  console.log('Testing changed files')
+  execSync('yarn test', { stdio: 'inherit' })
+} else {
+  console.log('No files to test')
+}

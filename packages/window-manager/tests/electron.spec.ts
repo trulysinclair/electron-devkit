843bbd919a70ba9e99ceff66fc1f4038cf9c48d2
Sinclair <truly.sinclair@gmail.com>
2024-10-26 21:01:47 -0400

    chore: document basic testing with Electron

--- packages/window-manager/tests/electron.spec.ts
+++ packages/window-manager/tests/electron.spec.ts
@@ -1,6 +1,55 @@
-import {_electron, test} from "@playwright/test";
+import { _electron, test } from "@playwright/test";
 
-test('launch app', async () => {
-  const electronApp = await _electron.launch({args: ['dist/main.cjs']})
-  await electronApp.close()
-})
+test("launch app", async () => {
+  const electronApp = await _electron.launch({
+    args: ["dist/main.cjs"],
+  });
+
+  // Electron v29 -----------
+  // --- Classes
+  // BrowserWindow - only window class
+  // BrowserView - only view class
+
+  // --- Window Methods
+  // BrowserWindow.fromId(id): BrowserWindow | null - get a window by its ID
+  // BrowserWindow.getAllWindows(): BrowserWindow[] - get all windows
+  // BrowserWindow.getFocusedWindow(): BrowserWindow | null - get the focused window
+  // BrowserWindow.fromBrowserView(browserView): BrowserWindow | null - get a window using a browser view instance
+  // BrowserWindow.fromWebContents(webContents): BrowserWindow | null - get a window using a webContents instance
+
+  // --- View-based Methods
+  // BrowserWindow.getBrowserView(): BrowserView - get the BrowserView for the window
+  // BrowserWindow.getBrowserViews(): BrowserView[] - get all views for a window, sorted by z-index
+  // BrowserWindow.addBrowserView(browserView): void - support for multiple views
+  // BrowserWindow.setBrowserView(browserView): void - set the only view for the window
+  // BrowserWindow.removeBrowserView(browserView): void - remove a view from the window
+  // BrowserWindow.setTopBrowserView(browserView): void - move a view to the top of a stack
+  // ------------------------
+
+  // Electron v30+ ----------
+  // --- Classes
+  // BaseWindow - basic window with only views, no url direct loading
+  // BrowserWindow - a window with a primary contentView, with support for additional views
+  // View - create and layout native views with no webContents
+  // WebContentsView - a View that displays a WebContents (entirely c++ based, cannot replicate)
+
+  // --- Window Methods
+  // BrowserWindow.fromId(id): BrowserWindow | null - get a window by its ID
+  // BrowserWindow.getAllWindows(): BrowserWindow[] - get all windows
+  // BrowserWindow.getFocusedWindow(): BrowserWindow | null - get the focused window
+  // BrowserWindow.fromWebContents(webContents): BrowserWindow | null - get a window using a webContents instance
+
+  // --- View Methods
+  // BrowserWindow.contentView: View
+  // View.addChildView(view)
+  // View.removeChildView(view)
+  // View.children: View[]
+  // ------------------------
+
+  // create view, then use Browser
+  const version = await electronApp.evaluate(async ({ BrowserWindow, app }) => {
+    return process.versions.electron;
+  });
+  console.log(version);
+  await electronApp.close();
+});
